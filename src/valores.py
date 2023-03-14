275e265771856548ea7bdd181373580ae12dcba5
Marcelo Antunes <antunes.marcelo@hotmail.com>
2023-03-14 14:05:41 -0300

    Modified files experiencia.py and valores.py

--- src/valores.py
+++ src/valores.py
@@ -1,7 +1,7 @@
 import numpy as np
 from wordcloud import WordCloud
 
-text = "resiliência ética aprendizado maturidade diversidade respeito compromisso equipe"
+text = "resiliência ética aprendizado maturidade diversidade respeito comprometimento coragem foco"
 
 x, y = np.ogrid[:600, :600]
 mask = (x - 300) ** 2 + (y - 300) ** 2 > 300 ** 2
@@ -9,4 +9,4 @@ mask = 255 * mask.astype(int)
 
 wc = WordCloud(mode="RGBA", background_color="rgba(0,0,0,0)", colormap="tab20", repeat=True, mask=mask)
 wc.generate(text)
-wc.to_file('./imagens/words2.png')
+wc.to_file('./img/words.png')
